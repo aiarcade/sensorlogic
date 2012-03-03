d5420b862513bc6e6096e23b6b033e75f679a19c
Mahesh.fisat <Mahesh.fisat@76830462-ed2f-d057-2fc6-5c69cd27ccb5>
2012-03-03 05:36:24 +0000

    Initial Import 2

--- sensorLogic.py
+++ sensorLogic.py
@@ -1,8 +1,34 @@
 import sys
-
 from PyQt4 import Qt
 import PyQt4.Qwt5 as Qwt
 import serial
+
+
+class mainPlot(Qt.QWidget):
+	def __init__(self, *args):
+		print "hello"
+		Qt.QWidget.__init__(self, *args)
+		self.layout=Qt.QGridLayout(self)
+		#Pressure graph
+		self.prsPlot=Qwt.QwtPlot(self)
+		self.prsPlot.setTitle('Pressure Sensor')
+		self.prsPlot.setCanvasBackground(Qt.Qt.white)
+		self.prsPlot.plotLayout().setCanvasMargin(0)
+		self.prsPlot.plotLayout().setAlignCanvasToScales(True)
+		self.layout.addWidget( self.prsPlot, 0, 0)
+		self.curveP1 = Qwt.QwtPlotCurve("P1")
+		self.curveP1.attach(self.prsPlot)
+		self.curveP2 = Qwt.QwtPlotCurve("P2")
+		self.curveP2.attach(self.prsPlot)
+		self.curveP3 = Qwt.QwtPlotCurve("P3")
+		self.curveP3.attach(self.prsPlot)
+		self.curveP4 = Qwt.QwtPlotCurve("P4")
+		self.curveP4.attach(self.prsPlot)
+		self.curveP5 = Qwt.QwtPlotCurve("P5")
+		self.curveP5.attach(self.prsPlot)
+		self.prsPlot.replot()
+        
+
 
 class sensor():
 	def __init__(self, device):
@@ -14,36 +40,15 @@ class sensor():
 		self.port.close()
 		
 
-class mainPlot(Qt.QWidget):
-	def __init__(self, *args):
-		Qt.QWidget.__init__(self, *args)
-		self.layout = Qt.QGridLayout(self)
-		self.prsPlot = Qwt.QwtPlot(self)
-        self.prsPlot.setTitle('Pressure Sensor')
-        self.prsPlot.setCanvasBackground(Qt.Qt.white)
-        self.prsPlot.plotLayout().setCanvasMargin(0)
-        self.prsPlot.plotLayout().setAlignCanvasToScales(True)
-        self.layout.addWidget( self.prsPlot, 0, 0)
-        self.prsPlot.replot()
+
         
-        
-def main(args):
-    app = Qt.QApplication(args)
-    demo = make()
-    sys.exit(app.exec_())
+app = Qt.QApplication(sys.argv)
+print "hello"
+demo = mainPlot()
+demo.resize(400, 600)
+demo.show()
+sys.exit(app.exec_())
 
-# main()
-
-
-def make():
-    demo = mainPlot()
-    demo.resize(400, 600)
-    demo.show()
-    return demo
-
-# make()
-
-
-# Admire!
-if __name__ == '__main__':
-    main(sys.argv)
+    
+    
+    
