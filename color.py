8cd8b52b33569784e4a36957f75c66df54a70131
Mahesh.fisat <Mahesh.fisat@76830462-ed2f-d057-2fc6-5c69cd27ccb5>
2013-06-25 12:46:57 +0000

    Multiple files on record -fixed

--- color.py
+++ color.py
@@ -1,12 +1,6 @@
 import serial
 
-port = serial.Serial('/dev/ttyUSB0',9600, timeout=1)
+port = serial.Serial('/dev/ttyUSB0',4200, timeout=1)
 while 1:
-	try:	
 		line=port.readline()
-		print line
-		
-
-	except KeyboardInterrupt:
-		break
 port.close()
